217da2e168b4df7801bbce1e632e823827cad3a9
jeyfredc <jeyfredc@gmail.com>
2022-05-04 11:07:41 -0500

    Se realiza instalación de paquetes para realizar analisis del proyecto

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPLugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
 	entry: './src/index.js',
@@ -12,7 +13,6 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
     resolve: {
         extensions : ['.js'],
         alias: {
@@ -77,5 +77,14 @@ module.exports = {
             ]
         }),
         new Dotenv(),
-    ]
+        new BundleAnalyzerPlugin(),
+    ],
+    devServer : {
+        static : path.join(__dirname, 'dist'),
+        watchFiles: path.join(__dirname, "./**"),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+        open: true 
+    },
 }
